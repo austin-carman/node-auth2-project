98b8bf000aa3f2803f6f5fdd8fc55bf5aecf3972
Austin Carman <abcarman12@gmail.com>
2021-06-15 15:04:20 -0600

    fleshed out only middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,9 +1,22 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const Users = require('../users/users-model');
+const jwt = require('jsonwebtoken');
 
 const restricted = (req, res, next) => {
-
-  next()
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401).json({ message: 'Token invalid' })
+      } else {
+        console.log('decoded token:', decoded);
+        req.decodedJwt = decoded;
+        next()
+      }
+    })
+  } else {
+    res.status(401).json({ message: 'Token required' });
+  }
   /*
     If the user does not provide a token in the Authorization header:
     status 401
@@ -21,8 +34,20 @@ const restricted = (req, res, next) => {
   */
 }
 
-const only = role_name => (req, res, next) => {
-  next()
+const only = role_name => (req, res, next) => { // this is a middleware builder
+  console.log(`desired: ${role_name}`);
+  console.log(`decoded: ${req.decodedJwt.role}`);
+  if (role_name === req.decodedJwt.role_name) {
+    console.log('role_name', role_name);
+    console.log('req.decodedJwt.role_name', req.decodedJwt.role_name);
+    next()
+  } else {
+    next({
+      status: 403,
+      message: 'This is not for you'
+    })
+  }
+
   /*
     If the user does not provide a token in the Authorization header with a role_name
     inside its payload matching the role_name passed to this function as its argument:
@@ -37,7 +62,7 @@ const only = role_name => (req, res, next) => {
 
 
 const checkUsernameExists = async (req, res, next) => {
-  try{
+  try {
     const user = await Users.findBy(req.body)
     if (user) {
       req.user = user;
@@ -68,11 +93,12 @@ const validateRoleName = (req, res, next) => {
       req.role_name = 'student'
       next()
     } else if (role_name.trim() === 'admin') {
-      next({ 
+      next({
         status: 422,
-        message: 'Role name can not be admin' })
+        message: 'Role name can not be admin'
+      })
     } else if (role_name.trim().length > 32) {
-        res.status(422).json({ message: 'Role name can not be longer than 32 chars' })
+      res.status(422).json({ message: 'Role name can not be longer than 32 chars' })
     } else if (role_name.trim()) {
       req.role_name = role_name.trim()
       next()
